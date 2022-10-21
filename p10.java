5b0a7ffcf16cbb7603ca6c1d2e44bdf38da5e7d9
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2022-10-21 18:07:02 +0600

    Create p10.java

---
+++ p10.java
@@ -0,0 +1,13 @@
+package noname1;
+
+import java.util.Scanner;
+
+public class p10
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner (System.in);
+        
+        
+    }
+}
